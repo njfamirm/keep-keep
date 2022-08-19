9fad514beddd213dd315235ae2e23896fdac91f3
S. Amir Najafi <njfamirm@gmail.com>
2022-08-20 02:03:53 +0430

    feat: add esbuild config

---
+++ esbuild.config.js
@@ -0,0 +1,36 @@
+const esbuild = require("esbuild");
+
+// const browserList = [
+//   "chrome58",
+//   // 'edge16', // not work!
+//   "firefox57",
+//   // 'ie11', // not work!
+//   // 'ios10', // not work!
+//   "opera45",
+//   "safari11",
+// ];
+
+const DEBUG_MODE = process.env.DEBUG_MODE === "debug" ? true : false;
+
+const esbuildOptionTypescript = {
+  entryPoints: ["dist/index.js"],
+  outfile: "./extension/src/index.js",
+  bundle: true,
+  minify: true,
+  sourcemap: DEBUG_MODE,
+  // target: browserList,
+  platform: "browser",
+};
+
+// const esbuildOptionCss = {
+//   entryPoints: ["src/global.css"],
+//   outfile: "static/global.css",
+//   bundle: true,
+//   minify: true,
+//   sourcemap: DEBUG_MODE,
+//   external: ["*.woff2"],
+// };
+
+esbuild.build(esbuildOptionTypescript);
+
+// esbuild.build(esbuildOptionCss);
